4dcddc4814e36bf0542d89f170f77bb34366f873
Justin Joseph <justinj2me@gmail.com>
2025-12-15 08:59:09 +0530

    Initial Commit

---
+++ AI-Calorie-Tracker/AI_Calorie_TrackerApp.swift
@@ -0,0 +1,21 @@
+//
+//  AI_Calorie_TrackerApp.swift
+//  AI-Calorie-Tracker
+//
+//  Created by Justin Joseph on 12/15/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct AI_Calorie_TrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ AI-Calorie-TrackerTests/AI_Calorie_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Calorie_TrackerTests.swift
+//  AI-Calorie-TrackerTests
+//
+//  Created by Justin Joseph on 12/15/25.
+//
+
+import Testing
+@testable import AI_Calorie_Tracker
+
+struct AI_Calorie_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
